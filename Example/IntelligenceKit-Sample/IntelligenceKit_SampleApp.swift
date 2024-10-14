0583d069b5c80aabf287f10157c70b6426d288fe
Michel-André Chirita <michel.chirita@gmail.com>
2024-10-14 18:00:47 +0200

    first commit

---
+++ Example/IntelligenceKit-Sample/IntelligenceKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligenceKit_SampleApp.swift
+//  IntelligenceKit-Sample
+//
+//  Created by Michel-Andre Chirita on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct IntelligenceKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
